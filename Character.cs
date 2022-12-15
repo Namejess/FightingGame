eeb8f263fcaaee64135ab74dd8a3b1fcd90cd8bc
Namejess <jessy_drouin@protonmail.com>
2022-12-15 10:51:54 +0100

    :sparkles: Feature Combat
    Ajout feature/combat

---
+++ Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightingGame
+{
+    interface Character
+    {
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+        public int Speed { get; set; }
+        public int CooldownSpecialAttack { get; set; }
+
+        public Character(int health, int attackPower, int speed, int cooldownSpecialAttack)
+        {
+            Health = health;
+            AttackPower = attackPower;
+            Speed = speed;
+            CooldownSpecialAttack = cooldownSpecialAttack;
+        }
+
+    }
+}
